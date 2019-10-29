ec01bd533590a8f8eb95170a34a0e940679f7cfe
Ozan Yıldız <developer@yildizozan.com>
2019-10-29 21:14:40 +0300

    Initial commit

---
+++ src/com/company/CurrentConditionsDisplay.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	private float temperature;
+	private float humidity;
+	private Observable observable;
+
+	public CurrentConditionsDisplay(Observable observable) {
+		this.observable = observable;
+		observable.addObserver(this);
+	}
+
+	@Override
+	public void display() {
+		System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+	}
+
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if (o instanceof WeatherData) {
+			WeatherData weatherData = (WeatherData) o;
+			this.temperature = weatherData.getTemperature();
+			this.humidity = weatherData.getHumidity();
+			display();
+		}
+	}
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay display = new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
